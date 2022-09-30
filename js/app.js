7996e35c2d81c1810764ec76507852f3ff405af4
Sanny-Belisario <sanny-belisario@educar.rs.gov.br>
2022-09-30 09:09:30 -0300

    api angular js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
+(function(){
 angular.module('blog', []);
 angular.module('blog').controller('Rest', function ($scope, $http){
   $http.get('https://api-fake-blog.herokuapp.com/postagens').
       success(function(data) {
           $scope.publicacoes = data;
+
       }
   );
 });
+
+angular.module('post', []);
+
+angular.module('post').controller('MyController', function ($scope, $http, $location){
+  var absUrl = $location.absUrl();
+  var url = absUrl.split("#/");
+  console.log(url);
+  $http({
+     method: 'GET',
+     url: 'https://api-fake-blog.herokuapp.com/postagem/'+url[1]
+  }).then(function (data){
+    $scope.publicacao = data.data;
+    console.log(data.data);
+    
+  },function (error){
+
+  });
+})
+})()
